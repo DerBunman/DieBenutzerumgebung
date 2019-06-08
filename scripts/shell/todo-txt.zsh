18d77d0a886351c1f0ca719b527cb9216f5e462e
DerBunman <lw200552@googlemail.com>
2019-06-08 08:09:33 +0200

    Moved todotxt install to scripts, removed no longer needed apt packages

--- scripts/shell/todo-txt.zsh
+++ scripts/shell/todo-txt.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 # DOTFILES_INFO: init:   clones some addons to ~/.todo/actions.d/
 # DOTFILES_INFO: update: updates these git repos
-# DOTFILES_RUN_ACTIONS: init
+# DOTFILES_RUN_ACTIONS:  init
+
+ # todotxt-cli is for ubuntu and todo-txt is for debian
+sudo apt-get install todo-txt
+sudo apt-get install todotxt-cli
 
 if [ "$1" = "init" ]; then
 	mkdir -p ~/.todo/actions.d/
