bff5621e894e03cb5f0133203c2c8630c4c96d78
DerBunman <lw200552@googlemail.com>
2019-06-10 18:17:26 +0200

    setxkbmap.zsh: error handling

--- data/bin/autostart/always/setxkbmap.zsh
+++ data/bin/autostart/always/setxkbmap.zsh
@@ -1,11 +1,22 @@
 #!/usr/bin/env zsh
 . ${0:A:h}/../../../../lib/config.zsh
 
-setxkbmap \
-	-rules $(conf get setxkbmap/rules || echo evdev) \
-	-model $(conf get setxkbmap/model || echo evdev) \
-	-layout $(conf get setxkbmap/layout || echo us) \
+trap on_error ERR
+function on_error() {
+	echo -e "\n\nThe failed commandline:"
+	echo         "======================"
+	echo "setxkbmap $args"
+
+	exit 1
+}
+
+args=(
+	-rules $(conf get setxkbmap/rules || echo evdev)
+	-model $(conf get setxkbmap/model || echo evdev)
+	-layout $(conf get setxkbmap/layout || echo us)
 	-variant $(conf get setxkbmap/variant || echo altgr-intl)
+)
+setxkbmap "${args[@]}"
 
 # todo
 #setxkbmap -option 'grp:shift_caps_toggle'
