7babacd415ec32ef4a4158696170933e2ced06bd
DerBunman <lw200552@googlemail.com>
2019-06-11 12:22:36 +0200

    setxkbmap.zsh now runs a script stored in conf

--- data/bin/autostart/always/setxkbmap.zsh
+++ data/bin/autostart/always/setxkbmap.zsh
@@ -1,23 +1,13 @@
 #!/usr/bin/env zsh
 . ${0:A:h}/../../../../lib/config.zsh
 
-trap on_error ERR
-function on_error() {
-	echo -e "\n\nThe failed commandline:"
-	echo         "======================"
-	echo "setxkbmap $args"
+tmp=$(mktemp)
 
+conf get setxkbmap/script > "$tmp" || {
+	echo "Error: conf is not set up. Please run:"
+	echo "dotfiles configure input"
 	exit 1
 }
 
-args=(
-	-rules $(conf get setxkbmap/rules || echo evdev)
-	-model $(conf get setxkbmap/model || echo evdev)
-	-layout $(conf get setxkbmap/layout || echo us)
-	-variant $(conf get setxkbmap/variant || echo altgr-intl)
-)
-setxkbmap "${args[@]}"
-
-# todo
-#setxkbmap -option 'grp:shift_caps_toggle'
-#setxkbmap -option 'caps:swapescape'
+. "$tmp"
+rm -f "$tmp"
