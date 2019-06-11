c23e7e90e263d59e12b5d35e970912cd47b8c907
DerBunman <lw200552@googlemail.com>
2019-06-11 12:26:46 +0200

    conf: added rm command

--- modules/conf.zsh
+++ modules/conf.zsh
@@ -6,24 +6,26 @@ test -d "$storage_path" || mkdir -p "$storage_path"
 help=$({
 	cat <<-'EOF'
 	Syntax:
-	  conf command some/key
+		conf command some/key
 	
-	  the key has to be a valid path without leading slash
+		the key has to be a valid path without leading slash
 
 	Examples:
-	  put a key:
-	    echo de_DE | dotfiles config put system/keymap
-	  get a key:
-	    conf get system/keymap
-	  edit a key in $EDITOR (will create if non existant):
-	    conf edit system/keymap
-	EOF
+		put a key:
+			echo de_DE | conf put system/keymap
 
+		get a key:
+			conf get system/keymap
+
+		edit a key in $EDITOR (will create if non existant):
+			conf edit system/keymap
+
+	EOF
 })
 
 if [ "$2" = "" ]; then
-	echo "Error: provide key name"
 	echo $help
+	echo "Error: provide key name"
 	exit 1
 fi
 
@@ -54,5 +56,11 @@ if [ "$1" = "put" ]; then
 	cat - > "$key_path" || exit 1
 	exit
 fi
+
+if [ "$1" = "rm" ]; then
+	test -f "${key_path}" && rm -f "${key_path}"
+	exit
+fi
+
 
 echo $help
