db79f711bb86ee8827d0aa4247041abd278a12da
DerBunman <lw200552@googlemail.com>
2019-06-11 08:50:19 +0200

    conf: added edit command

--- modules/config.zsh
+++ modules/config.zsh
@@ -11,6 +11,8 @@ help=$({
 	echo "  echo de_DE | dotfiles config put system/keymap"
 	echo " get a key:"
 	echo "  dotfiles config get system/keymap"
+	echo " edit a key in \$EDITOR (will create if non existant):"
+	echo "  dotfiles config edit system/keymap"
 })
 
 if [ "$1" = "get" ]; then
@@ -22,8 +24,29 @@ if [ "$1" = "get" ]; then
 	test -f "$STORAGE/$2" || exit 1
 	cat "$STORAGE/$2"
 	exit
+fi
 
-elif [ "$1" = "put" ]; then
+[ -d "$STORAGE/$2" ] && {
+	echo "Error: $STORAGE/$2 exists but is a directory."
+	exit 1
+}
+
+if [ "$1" = "edit" ]; then
+	if [ "$2" = "" ]; then
+		echo "Error: provide key name"
+		echo $help
+		exit 1
+	fi
+	mkdir -p "$STORAGE/${2:h}"
+	if ! (( $+commands[$EDITOR] )) ; then
+		EDITOR=vim
+	fi
+	$EDITOR "$STORAGE/$2" || exit 1
+	exit
+fi
+
+
+if [ "$1" = "put" ]; then
 	if [ "$2" = "" ]; then
 		echo "Error: provide key name"
 		echo $help
@@ -32,7 +55,6 @@ elif [ "$1" = "put" ]; then
 	mkdir -p "$STORAGE/${2:h}"
 	cat - > "$STORAGE/$2" || exit 1
 	exit
-
-else
-	echo $help
 fi
+
+echo $help
