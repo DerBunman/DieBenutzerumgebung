1acd29094029d4c4c84f421e8f23e1aa52fc38ee
DerBunman <lw200552@googlemail.com>
2019-06-11 15:20:19 +0200

    added conf_chk_behavior

--- lib/config.zsh
+++ lib/config.zsh
@@ -2,3 +2,10 @@
 function conf() {
 	$HOME/.repos/dotfiles/modules/conf.zsh "$@"
 }
+
+
+function conf_chk_behavior() {
+	local behaviors_enabled=( $(conf get dotfiles/behaviors_enabled) )
+	[ "${behaviors_enabled[(r)$1]}" = "$1" ] && return 0 || return 1
+}
+
