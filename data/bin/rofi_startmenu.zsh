506e7decbd5780fc3124fb208d31222703aa46b8
DerBunman <lw200552@googlemail.com>
2019-07-07 11:44:28 +0200

    added roft menu prototype

---
+++ data/bin/rofi_startmenu.zsh
@@ -0,0 +1,84 @@
+#!/usr/bin/env zsh
+self="$0"
+if [[ "$#" -eq 0 ]]; then
+	#drun_icon='\uf013' # Gear
+	run_icon=$(echo '\uf013') # Gear
+	rofi \
+			-pid ~/.rofi.menu.pid \
+			-i \
+			-show-icons \
+			-location 7 \
+			-yoffset -30 \
+			-width 520 \
+			-no-sidebar-mode \
+			-no-lazy-grab \
+			-display-run "$run_icon cmd" \
+			-display-drun "all apps" \
+			-kb-mode-next "Control+Alt+space" \
+			-run-command "{cmd}" \
+			-drun-match-fields "categories,name" \
+			-show mymenu \
+			-modi mymenu:"$0 show",drun
+	exit
+fi
+echo "$*" $X 1>&2
+shift
+
+if [ "$1" = "quit" ]; then
+    exit 0
+fi
+
+categories=$(grep Categories \
+	/usr/share/applications/*.desktop \
+	~/.local/share/applications/*desktop \
+	| cut -d'=' -f2 \
+	| sed 's/;/\n/g' \
+	| grep -v "^$\|^X-" \
+	| sed 's/^/» /g' \
+	| LC_COLLATE=POSIX sort --ignore-case --unique)
+
+if [ "$1" = "Application Categories" ]; then
+	echo -en "\x00prompt\x1f$1\n"
+	echo "back"
+	echo "${categories}"
+	echo "quit"
+
+elif [ "${1[1]}" = "»" ]; then
+	xdotool search --class rofi key --delay 0 --clearmodifiers 'Control+Alt+space'
+	xdotool search --class rofi type --delay 1 "${1:2}"
+
+	echo "swag"
+
+	# delete all input
+	#xdotool search --class rofi key --delay 0 'Control+w'
+
+#	echo "asdfasda"
+#	category="${1:2}"
+#	kill $(cat ~/.rofi.menu.pid)
+#	rofi \
+#		-pid ~/.rofi.menu.pid \
+#		-i \
+#		-show-icons \
+#		-location 7 \
+#		-yoffset -30 \
+#		-width 520 \
+#		-filter "${1:2}" \
+#		-sidebar-mode \
+#		-display-run "$run_icon cmd" \
+#		-display-drun "all apps" \
+#		-show drun \
+#		-modi mymenu:"$0 show",run
+
+else
+	icon='\uf015' # Home
+	echo -en "\x00prompt\x1f$icon main menu\n"
+	#echo -en "\0urgent\x1f0,2\n"
+	echo -en "\0active\x1f1\n"
+	echo -en "\0markup-rows\x1ftrue\n"
+	#echo -en "\0message\x1fSpecial <b>bold</b>message\n"
+
+	echo -en "Application Categories\0icon\x1ffolder\n"
+	echo -en "aa---p\0icon\x1ffolder\0cmd\x1fecho foo\n"
+	echo -en "{cmd}foo:<i>asdf</i>;\0cmd\x1fvlc\0icon\x1faudacious\n"
+	echo -en "\0message\x1fSpecial <b>bold</b> message\n"
+fi
