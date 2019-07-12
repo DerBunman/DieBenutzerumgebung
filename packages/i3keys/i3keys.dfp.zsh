1574d847b05858b8f064d44b0c6770bdf547b0ea
DerBunman <lw200552@googlemail.com>
2019-07-12 15:06:25 +0200

    added i3keys package

---
+++ packages/i3keys/i3keys.dfp.zsh
@@ -0,0 +1,53 @@
+#!/usr/bin/env zsh
+setopt MULTI_FUNC_DEF
+
+typeset -r info="$(<<-EOF
+	This package will install i3keys.
+	EOF
+	)"
+
+typeset -r info_short="i3keys package"
+
+typeset -r package_file=$0
+typeset -r package_name=${0:t:r:r}
+
+
+typeset -r -i version=1
+
+typeset -r license="TEST"
+
+# these packages have to be installed on your system
+# there may be differences between ubuntu and debian
+typeset -r -a packages_debian=(
+	golang-go
+)
+typeset -r -a packages_ubuntu=( ${(@)packages_debian} )
+
+# these are the dotfiles packages (dfp)
+# this package depends on
+typeset -r -a dfp_dependencies=(
+	i3
+)
+
+typeset -r -a host_flags=(
+	has_x11
+)
+
+# these symlinks will be created
+typeset -r -A symlinks=(
+)
+
+# the update/init function
+update init() {
+	install_dependencies_apt
+
+	go get -u github.com/RasmusLindroth/i3keys
+	cd $HOME/go/src/github.com/RasmusLindroth/i3keys
+
+	go install
+}
+
+# this will be called afer init/update
+# to veryfy that the installation is complete
+tests() {
+}
