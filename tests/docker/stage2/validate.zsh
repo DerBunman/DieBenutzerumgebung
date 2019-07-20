84140f2edb6277ed652dbcf7fcaebecce4282553
DerBunman <lw200552@googlemail.com>
2019-07-20 12:18:37 +0200

    docker tests: changed validation script so it should run on disco

--- tests/docker/stage2/validate.zsh
+++ tests/docker/stage2/validate.zsh
@@ -1,14 +1,21 @@
 #!/usr/bin/env zsh
 cd "$HOME" || exit
 echo > /scriptout.txt
-tail -f /scriptout.txt &
+#tail -f /scriptout.txt &
+
+pkill -9 -i xvfb
+sleep 1
 
 echo "========================================="
-xvfb-run -n 99 \
+#xvfb-run -n 99 \
+xvfb-run \
 	--server-args="-screen 0 1360x768x16" \
 	--auth-file ~/.Xauthority \
-	xterm -e "/validate2.zsh | tee /scriptout.txt" #&
+	xterm -e "xrdb -load ~/.Xresources; /validate2.zsh > /scriptout.txt" #&
 
+echo "========================================="
+
+cat /scriptout.txt
 echo "========================================="
 
 perceptualdiff -verbose -threshold 25000 neofetch1.png /validate.png
