8b906d90603d1a1b604863661b78611ef3947bf4
DerBunman <lw200552@googlemail.com>
2019-11-16 16:41:59 +0100

    ansible headers and some cleanup

--- roles/common/files/dotfiles/.zshrc.d/50-scratch.zsh
+++
@@ -1,8 +0,0 @@
-function scratch {
-    cur_dir="$HOME/scratch"
-    new_dir="/tmp/scratch-`date +'%s'`"
-    mkdir -p $new_dir
-    ln -nfs $new_dir $cur_dir
-    cd $cur_dir
-    echo "New scratch dir ready for grinding ;>"
-}
--- roles/common/files/dotfiles/.zshrc.d/99-kmpv.zsh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env zsh
-kmpv() { cat "$1" | ssh -t 192.168.178.95 -l khadas "DISPLAY=:0.0 mpv -fs --msg-level all=warn -cache 10024 --force-seekable=yes -" }  
-
