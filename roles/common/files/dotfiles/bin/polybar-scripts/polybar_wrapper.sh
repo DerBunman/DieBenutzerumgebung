a8c2eab4473af77ea3f5795c122e50fce1be138a
DerBunman <lw200552@googlemail.com>
2019-12-07 09:34:15 +0100

    made polybar configureable and some cleanup

--- roles/common/files/dotfiles/bin/polybar-scripts/polybar_wrapper.sh
+++ roles/common/files/dotfiles/bin/polybar-scripts/polybar_wrapper.sh
@@ -10,9 +10,10 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do
 	sleep 1
 done
-set -a
-. ~/.config/polybar/local_config.env
-set +a
+
+#set -a
+#. ~/.config/polybar/local_config.env
+#set +a
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	MONITOR=$m polybar --reload bottom &
